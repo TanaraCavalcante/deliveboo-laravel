331c9b9d490b2c731b515a411740fae85c474785
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-03 16:36:31 +0100

    adjusteds

--- app/Models/OrderPlate.php
+++ app/Models/OrderPlate.php
@@ -9,15 +9,15 @@ class OrderPlate extends Model
 {
     use HasFactory;
 
-     //! Relation "many to one" with Plate
-     public function plates()
-     {
-         return $this->hasMany(Plate::class);
-     }
-
      //! Relation "many to one" with Order
      public function orders()
      {
          return $this->hasMany(Order::class);
+     }
+
+     //! Relation "many to one" with Plate
+     public function plates()
+     {
+         return $this->hasMany(Plate::class);
      }
 }
