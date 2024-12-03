6d22df185d0222505705de41a146242c54175dbb
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-03 10:57:08 +0100

    RF2- implemneted basci structure to index method

--- app/Http/Controllers/Api/PlateController.php
+++ app/Http/Controllers/Api/PlateController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plate;
 use Illuminate\Http\Request;
 
 class PlateController extends Controller
 {
     public function index(){
-
+        $plates = Plate::all();
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $plates,
+            ]
+        );
     }
     public function show(){
-        
+
     }
 }
