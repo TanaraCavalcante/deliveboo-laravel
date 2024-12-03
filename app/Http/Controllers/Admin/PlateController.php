abe4c3e09576767c4596ea63f05a841fab88e5f7
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-03 12:28:22 +0100

    RF2- created method index for admin controller with pagination

--- app/Http/Controllers/Admin/PlateController.php
+++ app/Http/Controllers/Admin/PlateController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plate;
 use Illuminate\Http\Request;
 
 class PlateController extends Controller
 {
     public function index(){
+        $plate = Plate::paginate(10);
+        return view('admin.plates.index', compact('plates'));
 
     }
     public function create(){
