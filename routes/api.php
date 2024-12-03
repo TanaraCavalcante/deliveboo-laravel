d7804ddb397a28ef562a96712576c19b34aee66b
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-03 10:57:29 +0100

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\PlateController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(PlateController::class)->name('api.')->group(function(){
+    Route::get('plates', 'index')->name('plates.index');
+    Route::get('plates/{plate}', 'show')->name('plates.show');
 });
