7dfbf5b5954db5c4a3f91eb60a32ddead887df3e
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-04 10:34:56 +0100

    workin' on responsiveness

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -2,8 +2,8 @@
 @section("page-title", "Lista dei piatti")
 
 @section("content")
-<h1>Lista dei piatti:</h1>
-<div class="container">
+<h1 class="m-3">Lista dei piatti:</h1>
+<div class="container-fluid">
 
 <div class="row">
     <div class="col-12">
@@ -11,18 +11,18 @@
             <a href="{{route("admin.plates.create")}}" class="btn btn-sm btn-primary me-1 m-1">Crea un nuovo piatto!</a>
 
         </div>
-    <div class="table-responsive">
+    <div class="table-responsive-sm">
 <table class="table table-hover table-striped "> 
     
     <thead>
       <tr>
         <th scope="col">#</th>
         <th scope="col">Piatto</th>
-        <th scope="col">Descrizione</th>
+        <th scope="col" class="d-none d-lg-table-cell">Descrizione</th>
         <th scope="col">Prezzo</th>
-        <th scope="col">Ingredienti</th>
+        <th scope="col"  class="d-none d-lg-table-cell">Ingredienti</th>
         <th scope="col">Disponibilità</th>
-        <th scope="col">Immagine</th>
+        <th scope="col"  class="d-none d-lg-table-cell">Immagine</th>
         <th scope="col">Azioni</th>
         
       </tr>
@@ -34,11 +34,11 @@
       <tr>
         <td>{{$plate->id}}</td>
         <td>{{$plate->name}}</td>
-        <td>{{$plate->description}}</td>
+        <td class="d-none d-lg-table-cell">{{$plate->description}}</td>
         <td>{{$plate->price}}</td>
-        <td>{{$plate->ingredients}}</td>
+        <td  class="d-none d-lg-table-cell">{{$plate->ingredients}}</td>
         <td>{{$plate->visibility}}</td>
-        <td>{{$plate->image}}</td>
+        <td  class="d-none d-lg-table-cell">{{$plate->image}}</td>
         
     
         <td>
