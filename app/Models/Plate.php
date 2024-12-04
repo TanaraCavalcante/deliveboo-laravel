9c38cbdc1fbbd0ab3d2afe2e17bdfb65a110ca16
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-04 10:34:15 +0100

    added relationship manyToMany Plate to Orders

--- app/Models/Plate.php
+++ app/Models/Plate.php
@@ -18,15 +18,7 @@ class Plate extends Model
         'image'
     ];
 
-     //! Relation "one to Many" with OrderPlate
-     public function orderPlate()
-     {
-         return $this->belongsTo(OrderPlate::class);
-     }
-
-    //! Relation "many to one" with Restaurant
-    public function restaurants()
-    {
-        return $this->hasMany(Restaurant::class);
-    }
+    public function orders(){
+        return $this->belongsToMany(Order::class);
+      }
 }
