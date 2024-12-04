02ee49162e93acc553cf69b7a20c7ae54f93b74a
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-04 14:39:39 +0100

    index controller

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
@@ -11,7 +12,8 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        //
+        $restaurantes = Restaurant::all();
+        return view("", compact("restaurants")); //! view di andrea
     }
 
     /**
