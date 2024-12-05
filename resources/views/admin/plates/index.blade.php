9b32962c910e94df3bf381ae1ec84e61abb2797f
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-05 15:28:25 +0100

    RF2-eliminated info plus. it remains only the basic info

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -3,29 +3,22 @@
 
 @section('content')
     <h1 class="m-3">Lista dei piatti:</h1>
-    <div class="container-fluid">
-
+    <div class="container">
         <div class="row">
             <div class="col-12">
                 <div class="mb-3">
                     <a href="{{ route('admin.plates.create') }}" class="btn btn-sm btn-outline-primary me-1 m-1">Crea un nuovo
                         piatto!</a>
-
                 </div>
                 <div class="table-responsive-sm">
                     <table class="table  table-hover table-striped ">
-
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Piatto</th>
-                                <th scope="col" class="d-none d-lg-table-cell">Descrizione</th>
                                 <th scope="col">Prezzo</th>
-                                <th scope="col" class="d-none d-sm-table-cell">Ingredienti</th>
                                 <th scope="col">Disponibilità</th>
-                                <th scope="col" class="d-none d-lg-table-cell">Url immagine</th>
                                 <th scope="col">Azioni</th>
-
                             </tr>
                         </thead>
                         <tbody>
@@ -33,13 +26,8 @@
                                 <tr>
                                     <td>{{ $plate->id }}</td>
                                     <td>{{ $plate->name }}</td>
-                                    <td class="d-none d-lg-table-cell">{{ $plate->description }}</td>
                                     <td>{{ $plate->price }} </td>
-                                    <td class="d-none d-sm-table-cell">{{ $plate->ingredients }}</td>
                                     <td>{{ $plate->visibility }}</td>
-                                    <td class="d-none d-lg-table-cell">{{ $plate->image }}</td>
-
-
                                     <td>
                                         <a href="{{ route('admin.plates.show', $plate) }}"
                                             class="btn btn-sm btn-outline-info m-2 me-1">Mostra</a>
@@ -55,7 +43,6 @@
                                         </form>
                                     </td>
                                 </tr>
-
                             @empty
                                 <tr>
                                     <td colspan="11">Non ci sono piatti disponibili al momento...
