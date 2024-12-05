b1b6641ee690c8a6282d7f9effd3230d1e381792
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-05 17:42:04 +0100

    RF3- modified logo to nullable in migration for type table

--- database/migrations/2024_12_02_140301_create_types_table.php
+++ database/migrations/2024_12_02_140301_create_types_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->string('logo', 255);
+            $table->string('logo', 255)->nullable();
             $table->timestamps();
         });
     }
