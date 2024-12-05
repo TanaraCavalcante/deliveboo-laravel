b88b02a209f4f8ab468725705c0ad1d37a54b8ef
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-05 14:19:49 +0100

    RF2-fixed error typing ops

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -8,57 +8,60 @@
                 <h1 class="m-3">Menù:</h1>
             </div>
             <div>
-                @if (count($plates === 0))
+                @if (count($plates) === 0)
                     <div>
                         <h3>Benvenuto nel tuo pannello di controllo</h3>
                         <p>Inserisci i tuoi piatti cliccando nel tasto qui sotto</p>
-                        <a href="{{ route('admin.plates.create') }}" class="btn btn-sm btn-outline-primary me-1 m-1">Crea un nuovo piatto!</a>
+                        <a href="{{ route('admin.plates.create') }}" class="btn btn-sm btn-outline-primary me-1 m-1">Crea un
+                            nuovo piatto!</a>
                     </div>
                 @else
-
+                    <div class="col-12">
+                        <div class="mb-3">
+                            <a href="{{ route('admin.plates.create') }}" class="btn btn-sm btn-outline-primary me-1 m-1">Crea
+                                un nuovo piatto!</a>
+                        </div>
+                        <div class="table-responsive-sm">
+                            <table class="table table-hover table-striped ">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Piatto</th>
+                                        <th scope="col">Prezzo</th>
+                                        <th scope="col">Disponibilità</th>
+                                        <th scope="col">Azioni</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($plates as $index => $plate)
+                                        <tr>
+                                            <td>{{ $plate->name }}</td>
+                                            <td>{{ $plate->price }} </td>
+                                            <td>{{ $plate->visibility }}</td>
+                                            <td class="d-flex">
+                                                <a href="{{ route('admin.plates.show', $plate) }}"
+                                                    class="btn btn-sm btn-outline-info m-2 me-1">Mostra Dettagli</a>
+                                                <a href="{{ route('admin.plates.edit', $plate) }}"
+                                                    class="btn btn-sm btn-outline-success m-2 me-1">Modifica</a>
+                                                <form action="{{ route('admin.plates.delete', $plate) }}" method="POST"
+                                                    class="plate-destroyer" custom-data-name="{{ $plate->name }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit"
+                                                        class="btn btn-sm btn-outline-warning m-2">Cancella</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="11">Non ci sono piatti disponibili al momento...
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 @endif
-            </div>
-            <div class="col-12">
-                <div class="mb-3">
-                    <a href="{{ route('admin.plates.create') }}" class="btn btn-sm btn-outline-primary me-1 m-1">Crea un nuovo piatto!</a>
-                </div>
-                <div class="table-responsive-sm">
-                    <table class="table table-hover table-striped ">
-                        <thead>
-                            <tr>
-                                <th scope="col">Piatto</th>
-                                <th scope="col">Prezzo</th>
-                                <th scope="col">Disponibilità</th>
-                                <th scope="col">Azioni</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($plates as $index => $plate)
-                                <tr>
-                                    <td>{{ $plate->name }}</td>
-                                    <td>{{ $plate->price }} </td>
-                                    <td>{{ $plate->visibility }}</td>
-                                    <td class="d-flex">
-                                        <a href="{{ route('admin.plates.show', $plate) }}" class="btn btn-sm btn-outline-info m-2 me-1">Mostra Dettagli</a>
-                                        <a href="{{ route('admin.plates.edit', $plate) }}" class="btn btn-sm btn-outline-success m-2 me-1">Modifica</a>
-                                        <form action="{{ route('admin.plates.delete', $plate) }}" method="POST"
-                                            class="plate-destroyer" custom-data-name="{{ $plate->name }}">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit"
-                                                class="btn btn-sm btn-outline-warning m-2">Cancella</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="11">Non ci sono piatti disponibili al momento...
-                                    </td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
             </div>
         </div>
     </div>
