fb5b59c11de89b318b4a703e6583e16b88afd9e1
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-05 15:47:21 +0100

    RF2- added first messagge welcome

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -8,6 +8,10 @@
                 <h1 class="m-3">Lista dei piatti:</h1>
             </div>
             <div class="col-12">
+                <div class="alert alert-success tex-center">
+                    <h3>Benvenuto nel tuo pannello di controllo</h3>
+
+                </div>
                 <div class="mb-3">
                     <a href="{{ route('admin.plates.create') }}" class="btn btn-sm btn-outline-primary me-1 m-1">Crea un nuovo
                         piatto!</a>
