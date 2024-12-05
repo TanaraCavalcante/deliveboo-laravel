1dfe886cb95afe4a5a5579d0555d469022a1f1fa
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-05 14:06:53 +0100

    RF2- elimnated link url image

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -5,7 +5,6 @@
         <div class="row justify-content-center">
             <div class="card col-12 col-md-8 col-lg-6  p-4">
                 <div class="card-body">
-                    {{-- dopo inserimento modifiche per aggiunta immagine utente --}}
                     {{-- <img src="{{ $plate->image}}" class="card-img-top" alt="{{ $plate->name }}"> --}}
                     <h1 class="card-title  fw-bold">
                         Piatto: {{ $plate->name }}
@@ -22,14 +21,6 @@
                     <h6 class="card-text fw-bold">
                         Prezzo:{{ $plate->price }}
                     </h6>
-                    @if (!empty($plate->image))
-                      
-                    @else
-                        <div>
-                            {{-- valutare insieme se inserire una img placeholder --}}
-                            <p>Immagine non disponibile</p>
-                        </div>
-                    @endif
                 </div>
             </div>
         </div>
