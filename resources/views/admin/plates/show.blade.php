b2aea5e46885dbb32eb688cb5be85169d90e5277
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-05 15:24:17 +0100

    RF2- fixed some errors in show blade, added more info to be displayed

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -1,37 +1,28 @@
 @extends('layouts.app')
 
-
-
-@section("content")
-<h1>{{$plate->name}}</h1>
-<div class="container">
-    <div class="row justify-content-center">
-
-
-        <div class="card col-12 col-md-8 col-lg-6  p-4">
-
-            <div class="card-body">
-
-
-
-                    <h1 class="card-title  fw-bold">Piatto: {{ $plate->name }}</h1>
-
-                    <p class="card-title">Descrizione: {{ $plate->description }}</p>
-
-                <h6 class="card-title fw-bold">Prezzo:{{ $plate->price }}</h6>
-
-                <div>Link: <a href="{{ $plate->image }}" target="_blank">{{ $plate->image }}</a></div>
-
-
-
-
-
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-12 col-md-8 col-lg-6  p-4">
+                <div class="card-body">
+                    {{-- <img src="{{ $plate->image}}" class="card-img-top" alt="{{ $plate->name }}"> --}}
+                    <h1 class="card-title  fw-bold">
+                        Piatto: {{ $plate->name }}
+                    </h1>
+                    <p class="card-text">
+                        Descrizione piatto: {{ $plate->description }}
+                    </p>
+                    <p class="card-text">
+                        Ingredienti: {{ $plate->ingredients }}
+                    </p>
+                    <p class="card-text">
+                        Disponibilità del piatto: {{ $plate->visibility }}
+                    </p>
+                    <h6 class="card-text fw-bold">
+                        Prezzo:{{ $plate->price }}
+                    </h6>
+                </div>
             </div>
-          </div>
-
         </div>
     </div>
-
-</div>
-
 @endsection
