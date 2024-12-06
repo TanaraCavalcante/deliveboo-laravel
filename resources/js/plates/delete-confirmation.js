7da56892f5a5294665d087d28202783a8603e16e
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-06 17:21:25 +0100

    added js modal

--- resources/js/plates/delete-confirmation.js
+++ resources/js/plates/delete-confirmation.js
@@ -1,16 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const deleteButtons = document.querySelectorAll('.delete');
-    const plateNameElement = document.getElementById('plateName');
-    const plateIdInput = document.getElementById('plateId');
-    const deleteForm = document.getElementById('deleteForm');
+document.addEventListener("DOMContentLoaded", () => {
+    const deleteButtons = document.querySelectorAll(".delete");
+    const plateNameElement = document.getElementById("plateName");
+    const plateIdInput = document.getElementById("plateId");
+    const deleteForm = document.getElementById("deleteForm");
 
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const plateId = button.getAttribute('data-id');
-            const plateName = button.getAttribute('data-name');
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const plateId = button.getAttribute("data-id");
+            const plateName = button.getAttribute("data-name");
             plateNameElement.textContent = plateName;
             plateIdInput.value = plateId;
-
 
             deleteForm.action = `/admin/plates/${plateId}`;
         });
