e1ee768a4d15179d2ba7d16fbabec743e93ef210
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-06 13:05:25 +0100

    changes in show

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -9,10 +9,11 @@
         <div class="card col-12 col-md-8 col-lg-6 p-4 my-5">
             <div class="card-body">
                 <h1 class="card-title  fw-bold"> {{ $plate->name }}</h1>
-                <div class="card" style="width: 18rem;">
+                <div class="card mb-2" style="width: 18rem;">
                 <img src="{{asset("/storage/" . $plate->image)}}" alt="{{$plate->name}}" class="card-img-top">
                 </div>
                 <p class="card-title">{{ $plate->description }}</p>
+                <p class="card-title">Ingredients: {{ $plate->ingredients }}</p>
                 <p class="card-title ">Prezzo: <strong>€ {{ $plate->price }}</strong></p>
             </div>
         </div>
