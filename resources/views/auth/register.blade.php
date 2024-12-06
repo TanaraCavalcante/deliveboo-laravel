eb2caf8584d611adea534d98be37967d19a5d086
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-06 15:38:24 +0100

    RF2-deleted autocomplete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">{{ __('Registrati') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}" autocomplete="off">
+                        <form method="POST" action="{{ route('register') }}">
                             @csrf
 
                             <div class="row mb-3">
