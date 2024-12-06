fc77c67a5955847a981858289472c2c5568c232b
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-12-06 15:53:06 +0100

    fix errors

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -95,24 +95,5 @@
 
 
 @section('additional-scripts')
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const deleteButtons = document.querySelectorAll('.delete');
-            const plateNameElement = document.getElementById('plateName');
-            const plateIdInput = document.getElementById('plateId');
-            const deleteForm = document.getElementById('deleteForm');
-
-            deleteButtons.forEach(button => {
-                button.addEventListener('click', () => {
-                    const plateId = button.getAttribute('data-id');
-                    const plateName = button.getAttribute('data-name');
-                    plateNameElement.textContent = plateName;
-                    plateIdInput.value = plateId;
-
-
-                    deleteForm.action = `/admin/plates/${plateId}`;
-                });
-            });
-        });
-    </script>
+    @vite('resources/js/posts/delete-confirmation.js');
 @endsection
