1f42859abc65228670f5841dcb4961f29cd88def
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-08 17:05:37 +0100

    convert image size from bytes to kb

--- resources/js/plates/validation-create.js
+++ resources/js/plates/validation-create.js
@@ -90,8 +90,8 @@ subBtn.addEventListener('click', function (e) {
         };
 
         let imgSize = imgInput.files[0].size;
-        const imgMbSize = imgSize / 2 / 2;
-        if (imgMbSize > 5) {
+        const imgKbSize = imgSize / 1024; //!Conversione di bytes per kb, cosi si uguala al back
+        if (imgKbSize > 250) {
             e.preventDefault();
             inputErrors.push(`L'immagine è troppo grande per essere inserita! Non deve superare i 5 MB di grandezza`)
         }
