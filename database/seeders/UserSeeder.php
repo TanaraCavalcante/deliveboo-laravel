97b4d4239581bba4b518d95b7e0fff5eeade1a9e
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-08 23:06:29 +0100

    RF3- took info for users from a file csv

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Functions\Helpers;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $userData = Helpers::getCsv(__DIR__ . '/user_seeder.csv');
+        foreach ($userData as $indice => $riga) {
+            if ($indice != 0) {
+                $newUser = new User();
+                $newUser->name = $riga[0];
+                $newUser->email = $riga[1];
+                $newUser->password = $riga[2];
+
+                $newUser->save();
+
+            }
+        }
+
     }
 }
