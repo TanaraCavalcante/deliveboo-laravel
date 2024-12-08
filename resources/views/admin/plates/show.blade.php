5bab1d965587494b165dfcaee45bbb4f9c0341bb
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-08 17:51:38 +0100

    show modify

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -4,17 +4,20 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="card col-12 col-md-8 col-lg-6 p-4">
-                <div class="card-body">
+
+                <div class="card-body ">
+                    <h1 class="card-title text-center fw-bold mt-2">
+                        Piatto: {{ $plate->name }}
+                    </h1>
+                    <div class="card mb-2" style="width: 18rem;">
                     @if ($plate->image)
                         <img src="{{ asset('/storage/' . $plate->image) }}" class="card-img-top" alt="{{ $plate->name }}">
                     @else
                         <img src="{{ asset('images/placeholder-plate.png') }}" class="card-img-top" alt="Placeholder">
                     @endif
-                    <h1 class="card-title text-center fw-bold mt-2">
-                        Piatto: {{ $plate->name }}
-                    </h1>
+                    </div>
                     <p class="card-text">
-                        Descrizione piatto: {{ $plate->description }}
+                        Descrizione Piatto: {{ $plate->description }}
                     </p>
                     <p class="card-text">
                         Ingredienti: {{ $plate->ingredients }}
@@ -22,7 +25,7 @@
                     <p class="card-text fw-bold">
                         Prezzo: <strong>€ {{ $plate->price }}
                     </p>
-                    <a href="{{ route('admin.plates.index') }}" class="btn btn-primary">Torna indietro</a>
+                    <a href="{{ route('admin.plates.index') }}" class="btn btn-primary">Lista completa</a>
                 </div>
             </div>
         </div>
