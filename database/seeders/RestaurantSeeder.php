d234dab95373c5a780e4a915435af63539ecc918
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-08 23:08:26 +0100

    RF3- taking info for restaurant from file csv and adding relationship with types names in file

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Functions\Helpers;
+use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +17,38 @@ class RestaurantSeeder extends Seeder
     {
         //vado a prendere i dati dal mio csv euso il metodo creato per aprirlo in lettura
         $restaurantData = Helpers::getCsv(__DIR__ . '/seeder_ristorante.csv');
+        foreach ($restaurantData as $indice => $riga) {
+            if ($indice != 0) {
+
+                $rawTypes = explode(",", $riga[4]);
+                $typesToBeAssociated = array();
+
+                foreach($rawTypes as $rawType) {
+                    // per pulire dagli spazi nel csv che legge
+                    $cleanedType = trim($rawType);
+                    // per vedere in console cosa fa il seeder
+                    $this->command->comment('Looking for Type ' . $cleanedType);
+                    
+                    $type = Type::where('name', '=', $cleanedType)->firstOrFail();
+                    $typesToBeAssociated[] = $type;
+                }
+
+                $newRestaurant = new Restaurant();
+                $newRestaurant->user_id = $riga[1];
+                $newRestaurant->name = $riga[0];
+                $newRestaurant->address = $riga[2];
+                $newRestaurant->piva = $riga[3];
+
+                $newRestaurant->save();
+                // qui vado ad associare al restaurant id appena creato i type_id corrispondenti
+                // ai nomi dei tipi presenti nel csv
+                foreach($typesToBeAssociated as $typeToAssociate) {
+                    $newRestaurant->types()->attach($typeToAssociate->id);
+                }
+
+            }
+        }
+
 
     }
 }
