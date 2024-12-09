6f373b3674722761414ae579cbd87f78a62e6adf
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 11:43:22 +0100

    RF3- removed paginate from index plate

--- app/Http/Controllers/Api/PlateController.php
+++ app/Http/Controllers/Api/PlateController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class PlateController extends Controller
 {
     public function index(){
-        $plates = Plate::paginate(15);
+        $plates = Plate::all();
         return response()->json(
             [
                 "success" => true,
