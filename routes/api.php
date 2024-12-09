36a6612a3e0d43bb322acb47408f0ecf5ec27063
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 11:51:47 +0100

    RF3 - Routes for api/types

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\PlateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\PlateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::controller(PlateController::class)->name('api.')->group(function(){
     Route::get('plates', 'index')->name('plates.index');
     Route::get('plates/{plate}', 'show')->name('plates.show');
+});
+
+Route::controller(TypeController::class)->name('api.')->group(function(){
+    Route::get('types', 'index')->name('types.index');
+    Route::get('types/{type}', 'show')->name('types.show');
 });
