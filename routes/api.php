21b52f9f18acfcf07cbfe96756749f214bc5b79a
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 11:51:00 +0100

    RF3- added route api to restaurants

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PlateController;
+use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(PlateController::class)->name('api.')->group(function(){
-    Route::get('plates', 'index')->name('plates.index');
-    Route::get('plates/{plate}', 'show')->name('plates.show');
+    Route::get('plates','index')->name('plates.index');
+    Route::get('plates/{plate}','show')->name('plates.show');
+});
+
+
+
+
+
+
+
+
+
+
+Route::controller(RestaurantController::class)->name('api.')->group(function(){
+    Route::get('restaurants','index')->name('restaurants.index');
+    Route::get('restaurants/{restaurant}','show')->name('restaurants.show');
 });
