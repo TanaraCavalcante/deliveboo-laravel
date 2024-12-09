0bb66427d6c7d07d774da090714c1c54bd9b3a88
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-09 11:47:31 +0100

    RF3 - added for API for api/TypeController

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
-    //
+    public function index(){
+        $types = Type::all();
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $types,
+            ]
+        );
+    }
+    public function show(Type $type){
+        $type = Type::findOrFail($type->id);
+        return response()->json(
+            [
+                "success"=> true,
+                "results"=> $type,
+            ]
+        );
+
+    }
 }
