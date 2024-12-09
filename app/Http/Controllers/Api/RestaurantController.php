d48019d745c577d6c8af7f4a5d4a2e41ed68881a
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-09 11:46:32 +0100

    RF3- added index and show for restaurant api controller

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    //
+    public function index(){
+        $restaurants = Restaurant::all();
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $restaurants,
+            ]
+        );
+    }
+    public function show(Restaurant $restaurant){
+        $restaurant = Restaurant::findOrFail($restaurant->id);
+        return response()->json(
+            [
+                "success"=> true,
+                "results"=> $restaurant,
+            ]
+        );
+
+    }
 }
