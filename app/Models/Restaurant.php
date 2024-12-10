278f0c967681616c794c6f50f1f73f19adbf04f2
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-10 16:13:46 +0100

    added filterable in model

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Abbasudo\Purity\Traits\Filterable;
 
 class Restaurant extends Model
 {
     use HasFactory;
+    use Filterable;
 
     protected $fillable = [
         'user_id',
