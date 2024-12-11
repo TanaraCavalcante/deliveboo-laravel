8c432730250b2048cb9bdb17acbf051318e9ebc1
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-11 15:14:11 +0100

    added mailer config in env

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\StoreOrderRequest;
+use App\Mail\NewOrder;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -13,9 +14,9 @@ class OrderController extends Controller
 
     public function store(StoreOrderRequest $request){
         $data = $request->validated();
-        $order = Order::create($data);
+        $order = Order::create($data)->with('plates');
 
-        Mail::to('mailacuidevoinviare')->send("oggetto da inviare");
+        Mail::to('mailacuidevoinviare')->send(new NewOrder($order));
     }
 
 }
