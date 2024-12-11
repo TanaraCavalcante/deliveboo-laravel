7dc1266ca52978a4bdbd151a4a0425f376540737
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-11 13:14:47 +0100

    RF5 - Variable with the Braintree SANDBOX credentials on the controller

--- app/Http/Controllers/Api/BraintreeController.php
+++ app/Http/Controllers/Api/BraintreeController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class BraintreeController extends Controller
 {
-    //
+    protected $gateway;
+
+    //uso le variabile con le credenziale Braintree SANDBOX
+    public function __construct()
+    {
+        $this->gateway = new Gateway([
+            'environment' => config('braintree.environment'),
+            'merchantId' => config('braintree.merchant_id'),
+            'publicKey' => config('braintree.public_key'),
+            'privateKey' => config('braintree.private_key')
+        ]);
+    }
 }
