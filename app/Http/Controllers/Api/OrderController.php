090bcdf9df41530a5d99615a58ae8f26dc8ebea0
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-11 12:42:41 +0100

    generated NewOrder Mail

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\StoreOrderRequest;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class OrderController extends Controller
 {
@@ -13,6 +14,8 @@ class OrderController extends Controller
     public function store(StoreOrderRequest $request){
         $data = $request->validated();
         $order = Order::create($data);
+
+        Mail::to('mailacuidevoinviare')->send("oggetto da inviare");
     }
 
 }
