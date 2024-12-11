97a9923cb2e3d4408555e945b537c0471cf3efcc
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-11 15:52:25 +0100

    added json response to Controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -6,17 +6,33 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\StoreOrderRequest;
 use App\Mail\NewOrder;
 use App\Models\Order;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
 
-    public function store(StoreOrderRequest $request){
-        $data = $request->validated();
-        $order = Order::create($data)->with('plates');
+    public function store(Request $request){
+        $validator = Validator::make($request->all()[
+            /* inserire qui le regole di validazione */
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $order = Order::create($validator->validated())->with('plates');
 
         Mail::to('mailacuidevoinviare')->send(new NewOrder($order));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 
 }
