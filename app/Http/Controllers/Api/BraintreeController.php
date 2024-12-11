4faa80b6e1b43c093bc86142c5a09a96d37f78e8
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-11 13:31:32 +0100

    RF5 - function to process and validate payment

--- app/Http/Controllers/Api/BraintreeController.php
+++ app/Http/Controllers/Api/BraintreeController.php
@@ -26,4 +26,24 @@ class BraintreeController extends Controller
         $clientToken = $this->gateway->clientToken()->generate();
         return response()->json(['client_token' => $clientToken]);
     }
+
+     // Processa o pagamento
+     public function processPayment(Request $request)
+     {
+         $nonceFromClient = $request->payment_method_nonce; //nonce del metodo di pagamento
+         $amount = $request->amount; // valore della compra
+
+         $result = $this->gateway->transaction()->sale([
+             'amount' => $amount,
+             'paymentMethodNonce' => $nonceFromClient,
+             'options' => ['submitForSettlement' => true] //Submete a transação para processamento
+         ]);
+
+         //verifica se il pagamento è andato a buon fine dopo della riga 39
+         if ($result->success) {
+             return response()->json(['message' => 'Pagamento realizado com sucesso']);
+         } else {
+             return response()->json(['error' => $result->message], 500);
+         }
+     }
 }
