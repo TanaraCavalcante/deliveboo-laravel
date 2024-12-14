5d22e66072838adb8a44ec5b55375f9c0f6527bf
AndreaMargutti <marguttiandrea30@gmail.com>
2024-12-14 12:32:20 +0100

    problem with graph

--- resources/js/orders/stats.js
+++ resources/js/orders/stats.js
@@ -1,25 +1,26 @@
 import Chart from 'chart.js/auto';
 
 (async function () {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ];
+    const dates = [
+        ordersDate.forEach(singleDate => {
+            { year: singleDate }
+        })];
+
+    const totals = [
+        ordersTotal.forEach(singleTotal => {
+            { count: singleTotal }
+        })];
+
     new Chart(
         document.getElementById('myChart'),
         {
             type: 'bar',
             data: {
-                labels: data.map(row => row.year),
+                labels: dates.map(row => row.year),
                 datasets: [
                     {
                         label: 'Acquisitions by year',
-                        data: data.map(row => row.count)
+                        data: totals.map(row => row.count)
                     }
                 ]
             }
--- resources/views/admin/orders/stat.blade.php
+++ resources/views/admin/orders/stat.blade.php
@@ -17,8 +17,23 @@
 
 @section('additional-scripts')
 <script>
+    //passo gli ordini del ristorante da php a js
     let orders = @json($orders);
-    console.log(orders);
+    // console.log(orders);
+
+    // creo due array vuoti per le date e per i totali
+    let ordersDate = [];
+    let ordersTotal = [];
+
+    // per ogni ordine...
+    orders.forEach(order => {
+        //...recupero le date e le inserisco nell'array
+        const date = new Date(order.created_at);
+        ordersDate.push(date);
+        //...recupero i totali e li inserisco nell'array
+        ordersTotal.push(order.total);
+    });
+    console.log(ordersDate, ordersTotal);
 </script>
 
 @vite('resources/js/orders/stats.js');
