f6b16738b8dbad7c4041440ff74b01301f7cac94
AndreaMargutti <marguttiandrea30@gmail.com>
2024-12-14 17:40:56 +0100

    add problem with graphj

--- resources/js/orders/stats.js
+++ resources/js/orders/stats.js
@@ -1,20 +1,33 @@
-import Chart from 'chart.js/auto';
+import Chart from 'chart.js/auto'
 
 (async function () {
+    const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+    ];
 
     new Chart(
         document.getElementById('myChart'),
         {
-            type: 'line',
+            type: 'bar',
             data: {
-                labels: ordersDate.map(row => row.getMonth()),
+                labels: data.map(row => row.year),
                 datasets: [
                     {
                         label: 'Acquisitions by year',
-                        data: ordersTotal.map(row => row)
+                        data: data.map(row => row.count)
                     }
                 ]
             }
         }
     );
 })();
+
+
+
+
--- resources/views/admin/orders/stat.blade.php
+++ resources/views/admin/orders/stat.blade.php
@@ -16,24 +16,30 @@
 @endsection
 
 @section('additional-scripts')
+
 <script>
     //passo gli ordini del ristorante da php a js
     let orders = @json($orders);
     // console.log(orders);
 
-    // creo due array vuoti per le date e per i totali
-    let ordersDate = [];
-    let ordersTotal = [];
-
-    // per ogni ordine...
-    orders.forEach(order => {
-        //...recupero le date e le inserisco nell'array
-        const date = new Date(order.created_at);
-        ordersDate.push(date);
-        //...recupero i totali e li inserisco nell'array
-        ordersTotal.push(order.total);
+    let rawDatas = [];
+    orders.forEach(element => {
+        let singleData = {
+            year: new Date((element.created_at)).getFullYear(),
+            total: element.total
+        }
+        rawDatas.push(singleData);
     });
-    // console.log(ordersDate, ordersTotal);
+    console.log(rawDatas);
+
+    let years = [];
+
+    rawDatas.forEach(data => {
+
+    });
+
+
+
 </script>
 
 @vite('resources/js/orders/stats.js');
