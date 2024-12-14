07ecb0c471eb0bd1c4984f966107fc09d3ed867c
AlessioMartella7 <alessio.mart87@gmail.com>
2024-12-14 15:16:58 +0100

    changed name in mail.php

--- config/mail.php
+++ config/mail.php
@@ -99,7 +99,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'deliveboo@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'DeliveBoo'),
     ],
 
     /*
