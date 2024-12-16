9b6c9c89953a95ad0e8950715f904ec5c00703a1
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-12-16 21:42:59 +0100

    RF1-eliminated comma type error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     </script>
 
     <div class="container">
-        <div class="row justify-content-center">'
+        <div class="row justify-content-center">
 
             <div class="col-md-8">
                 <div class="card">
