9ede06f0a3e4580b10f9842988a5abc08ed2f967
AndreaMargutti <marguttiandrea30@gmail.com>
2024-12-18 11:42:48 +0100

    changed_localhost

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -29,6 +29,6 @@ class RestaurantController extends Controller
     // Function che porta al progetto vue
     public function redirectToVue()
     {
-        return redirect('http://localhost:5177/');
+        return redirect('http://localhost:5175/');
     }
 }
